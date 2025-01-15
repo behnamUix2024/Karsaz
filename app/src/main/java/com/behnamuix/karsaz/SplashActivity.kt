821a78c6e7b4a3351b44131def27f767938e2080
Behnam-6.403 <behnamuiux2023@gmail.com>
2025-01-15 15:54:25 -0800

    1403/10/26

--- app/src/main/java/com/behnamuix/karsaz/SplashActivity.kt
+++ app/src/main/java/com/behnamuix/karsaz/SplashActivity.kt
@@ -1,20 +1,32 @@
 package com.behnamuix.karsaz
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.LayoutInflater
+import android.view.WindowManager
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.behnamuix.karsaz.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_splash)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val binding=ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        binding.splashBtnWelcome.setOnClickListener(){
+            val intent=Intent(this@SplashActivity,MainActivity::class.java)
+            startActivity(intent)
         }
+
     }
 }
