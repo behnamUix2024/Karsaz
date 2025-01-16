066fb753e79944ba8ec87d3fa905bdae81a84c60
Behnam-6.403 <behnamuiux2023@gmail.com>
2025-01-15 16:20:34 -0800

    1403/10/26

--- app/src/main/java/com/behnamuix/karsaz/ui/fragments/AddFragment.kt
+++ app/src/main/java/com/behnamuix/karsaz/ui/fragments/AddFragment.kt
@@ -2,10 +2,7 @@ package com.behnamuix.karsaz.ui.fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import com.behnamuix.karsaz.R
+
 
 class AddFragment : Fragment() {
     // TODO: Rename and change types of parameters
